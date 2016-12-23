c6a8a7059f826e79d8fdc75db31102acb5672427
Glenn Mate <gmate@napower.com>
2016-12-23 14:07:50 -0500

    rest helpers

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -1,9 +1,30 @@
 package helpers
 
 import (
+        "net/http"
         "os"
         "github.com/Azure/go-autorest/autorest/azure"
 )
+
+// rest helpers
+
+func withInspection() autorest.PrepareDecorator {
+	return func(p autorest.Preparer) autorest.Preparer {
+		return autorest.PreparerFunc(func(r *http.Request) (*http.Request, error) {
+			fmt.Printf("Inspecting Request: %s %s\n", r.Method, r.URL)
+			return p.Prepare(r)
+		})
+	}
+}
+
+func byInspecting() autorest.RespondDecorator {
+	return func(r autorest.Responder) autorest.Responder {
+		return autorest.ResponderFunc(func(resp *http.Response) error {
+			fmt.Printf("Inspecting Response: %s for %s %s\n", resp.Status, resp.Request.Method, resp.Request.URL)
+			return r.Respond(resp)
+		})
+	}
+}
 
 // NewToken creates a token using the environmental vars passed during teamcity runs
 
